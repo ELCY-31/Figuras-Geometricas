0d51b75df2559bfead9959872fe3dd065fae8ebb
ELCY-31 <ed.ramoss@uea.edu.ec>
2026-01-12 00:44:08 +0100

    Semana

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejercicio1_Asignaturas
+{
+    class Curso
+    {
+        private List<string> asignaturas = new List<string>
+        {
+            "Matemáticas", "Física", "Química", "Historia", "Lengua"
+        };
+
+        public void MostrarAsignaturas()
+        {
+            Console.WriteLine("Asignaturas del curso:");
+            foreach (var a in asignaturas)
+                Console.WriteLine($"- {a}");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Curso curso = new Curso();
+            curso.MostrarAsignaturas();
+        }
+    }
+}
