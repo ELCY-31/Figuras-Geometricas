10ec4b31a270354f7bfea8f267f365d69e9eadc1
ELCY-31 <ed.ramoss@uea.edu.ec>
2026-01-12 01:10:28 +0100

    Semana 5

---
+++ Ejercicio2_Loteria/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ejercicio2_Loteria
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random rand = new Random();
+            HashSet<int> numeros = new HashSet<int>();
+
+            while (numeros.Count < 6)
+                numeros.Add(rand.Next(1, 50));
+
+            Console.WriteLine("Números de lotería:");
+            Console.WriteLine(string.Join(", ", numeros.OrderBy(n => n)));
+        }
+    }
+}
