ba5ec5241b40f8cd6499578db6004492984ac589
ELCY-31 <ed.ramoss@uea.edu.ec>
2026-01-12 01:11:26 +0100

    Semana 5

---
+++ Ejercicio3_Reprobadas/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ejercicio3_Reprobadas
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> notas = new List<int> { 70, 45, 90, 35, 30, 80, 60 };
+
+            int reprobados = notas.Count(n => n < 60);
+
+            Console.WriteLine("Notas: " + string.Join(", ", notas));
+            Console.WriteLine($"Reprobados: {reprobados}");
+        }
+    }
+}
