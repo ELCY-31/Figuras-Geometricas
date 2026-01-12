838186cba234b3cd0f86228421423e725fd775a0
ELCY-31 <ed.ramoss@uea.edu.ec>
2026-01-12 01:13:18 +0100

    Semana 5

---
+++ Ejercicio5_Estadistica/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ejercicio5_Estadistica
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<double> muestra = new List<double> { 8, 6, 7, 5, 9, 10 };
+
+            double media = muestra.Average();
+            double sumaCuadrados = muestra.Sum(x => Math.Pow(x - media, 2));
+            double desviacion = Math.Sqrt(sumaCuadrados / muestra.Count);
+
+            Console.WriteLine($"Media: {media:F2}");
+            Console.WriteLine($"Desviación típica: {desviacion:F2}");
+        }
+    }
+}
