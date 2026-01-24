c6d8886b38e5771ee5a96c147e4ac24054f200ca
ELCY-31 <ed.ramoss@uea.edu.ec>
2026-01-24 17:12:10 +0100

    Semana 7

---
+++ Pilas (stacks)/Ejercicio1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+class BalanceoParentesis
+{
+    static void Main()
+    {
+        string expresion = "{6 - [(4 + 9) - (2 * 7)] - (8 + 3) + 5}";
+        
+        if (EstaBalanceada(expresion))
+            Console.WriteLine("Fórmula balanceada.");
+        else
+            Console.WriteLine("Fórmula NO balanceada.");
+    }
+    static bool EstaBalanceada(string expresion)
+    {
+        Stack<char> pila = new Stack<char>();
+
+        foreach (char c in expresion)
+        {
+            // Si es símbolo de apertura, se apila
+            if (c == '(' || c == '{' || c == '[')
+            {
+                pila.Push(c);
+            }
+            // Si es símbolo de cierre
+            else if (c == ')' || c == '}' || c == ']')
+            {
+                // Si no hay nada para poder cerrar
+                if (pila.Count == 0)
+                    return false;
+
+                char ultimo = pila.Pop();
+
+                if (!EsPar(ultimo, c))
+                    return false;
+            }
+        }
+
+        // Si la pila está vacía, está balanceada
+        return pila.Count == 0;
+    }
+    static bool EsPar(char apertura, char cierre)
+    {
+        return (apertura == '(' && cierre == ')') ||
+               (apertura == '{' && cierre == '}') ||
+               (apertura == '[' && cierre == ']');
+    }
+}
---
+++ Pilas (stacks)/Ejercicio2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+class TorresDeHanoi
+{
+    static Stack<int> origen = new Stack<int>();
+    static Stack<int> entremedio = new Stack<int>();
+    static Stack<int> destino = new Stack<int>();
+    static void Main()
+{
+    int discos = 3;
+
+    for (int i = discos; i >= 1; i--)
+    origen.Push(i);
+    MostrarTorres();   // 
+    ResolverHanoi(discos, origen, destino, entremedio);
+}
+    static void ResolverHanoi(int n, Stack<int> origen, Stack<int> destino, Stack<int> entremedio)
+    {
+        if (n == 1)
+        {
+            destino.Push(origen.Pop());
+            MostrarTorres();
+            return;
+        }
+        ResolverHanoi(n - 1, origen, entremedio, destino);
+        destino.Push(origen.Pop());
+        MostrarTorres();
+        ResolverHanoi(n - 1, entremedio, destino, origen);
+    }
+    static void MostrarTorres()
+    {
+        Console.WriteLine("Origen: " + string.Join(", ", origen));
+        Console.WriteLine("Entremedio: " + string.Join(", ", entremedio));
+        Console.WriteLine("Destino: " + string.Join(", ", destino));
+        Console.WriteLine("---------------------------");
+    }
+}
